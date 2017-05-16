536fbf233228d8622d5e27e9d02b233c78339572
yuh <linyuhancs@gmail.com>
2017-05-16 01:49:36 -0400

    gcc-serial compile errors are fixed. However, because netferret-serial.c is still missing, netferret/paresc/gcc-serial.bldconf is copied from netferret/parsec/gcc-pthreads.bldconf

--- patch.sh
+++ patch.sh
@@ -1,3 +1,6 @@
 sed -i "s/=item \([0-9]\)/=item '\1'/g" $(find . -name "*.pod")
-cp ./bsd__types.h pkgs/libs/uptcpip/src/include/sys/bsd__types.h
-sudo apt-get install -y build-essential m4 x11proto-xext-dev libglu1-mesa-dev libxi-dev libxmu-dev libtbb-dev
+cp ./bsd__types.h ./pkgs/libs/uptcpip/src/include/sys/bsd__types.h
+cp ./netstreamcluster-gcc-serial.bldconf ./pkgs/netapps/netstreamcluster/parsec/gcc-serial.bldconf
+cp ./netdedup-gcc-serial.bldconf ./pkgs/netapps/netdedup/parsec/gcc-serial.bldconf
+cp ./netferret-gcc-serial.bldconf ./pkgs/netapps/netferret/parsec/gcc-serial.bldconf
+sudo apt-get install -y build-essential m4 x11proto-xext-dev libglu1-mesa-dev libxi-dev libxmu-dev libtbb-dev libssl-dev
